bd4818ae0a28ab9f040c4245778b99f8c305008c
Toky <herostoky@gmail.com>
2025-03-12 16:52:29 +0300

    Added repo and impl

---
+++ src/Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+internal interface IMovieRepository
+{
+    Task<bool> CreateMovieAsync(Movie movie, CancellationToken cancellationToken);
+    Task<Movie?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IEnumerable<Movie>> GetAllAsync(CancellationToken cancellationToken);
+    Task<bool> UpdateMovieAsync(Movie movie, CancellationToken cancellationToken);
+    Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ src/Movies.Application/Repositories/MovieRepository.cs
@@ -0,0 +1,43 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+internal class MovieRepository : IMovieRepository
+{
+    private readonly List<Movie> _movies = new();
+
+    public Task<bool> CreateMovieAsync(Movie movie, CancellationToken cancellationToken)
+    {
+        _movies.Add(movie);
+        return Task.FromResult(true);
+    }
+
+    public Task<Movie?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var movie = _movies.SingleOrDefault(m => m.Id.Equals(id));
+        return Task.FromResult(movie);
+    }
+
+    public Task<IEnumerable<Movie>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_movies.AsEnumerable());
+    }
+
+    public Task<bool> UpdateMovieAsync(Movie movie, CancellationToken cancellationToken)
+    {
+        var movieIndex = _movies.FindIndex(m => m.Id.Equals(movie.Id));
+        if (movieIndex == -1)
+        {
+            return Task.FromResult(false);
+        }
+
+        _movies[movieIndex] = movie;
+        return Task.FromResult(true);
+    }
+
+    public Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var removeCount = _movies.RemoveAll(m => m.Id.Equals(id));
+        return Task.FromResult(removeCount > 0);
+    }
+}
